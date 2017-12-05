5f5fe35d1dfb453726155f5e52c0019a5e9aa619
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-12-05 14:40:46 -0300

    Add project files.

---
+++ projetoBlog/Models/AcessoMongoDB.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projetoBlog.Models
+{
+    public class AcessoMongoDB
+    {
+
+        const string BANCO_DE_DADOS = "Blog";
+        const string USUARIOS = "Usuarios";
+        const string PUBLICACOES = "Publicacaoes";
+        public const string ConnectionString = "mongodb://localhost:27017";
+
+        public static IMongoCollection<Usuario> UsuariosCollection { get { return BancoDeDados.GetCollection<Usuario>(USUARIOS); } }
+
+        public static IMongoCollection<Publicacao> PublicacoesCollection { get { return BancoDeDados.GetCollection<Publicacao>(PUBLICACOES); } }
+
+        public static IMongoClient Client = new MongoClient(ConnectionString);
+
+        public static IMongoDatabase BancoDeDados = Client.GetDatabase(BANCO_DE_DADOS);
+
+    }
+}
